7b134373550f277457df0b57325437531dbf8424
YanBerdin <yandevformation@gmail.com>
2023-07-07 16:31:02 +0200

    update branch gh-pages

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.scss';
-import MeteoWidget from './MeteoWidget/MeteoWidget';
+import logo from "./logo.svg";
+import "./App.scss";
+import MeteoWidget from "./MeteoWidget/MeteoWidget";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-         WidgetMeteo
-        </h1>
+        <h1>WidgetMeteo</h1>
 
         <img src={logo} className="App-logo" alt="logo" />
 
@@ -21,10 +19,9 @@ function App() {
           Learn React
         </a> */}
       </header>
-      <MeteoWidget city="Marcilly" code={21200}></MeteoWidget>
+      <MeteoWidget city="Marcilly sur Tille" code={21200}></MeteoWidget>
       <MeteoWidget city="Dijon" code={21000}></MeteoWidget>
       <MeteoWidget city="Paris" code={75005}></MeteoWidget>
-
     </div>
   );
 }
