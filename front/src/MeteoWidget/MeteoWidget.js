af3e4024cb07003ec2cbc549b99631f80e454a8c
YanBerdin <yandevformation@gmail.com>
2023-07-10 15:29:44 +0200

    fixed padding

--- front/src/MeteoWidget/MeteoWidget.js
+++ front/src/MeteoWidget/MeteoWidget.js
@@ -47,7 +47,7 @@ function MeteoWidget({ city, code }) {
         <h3 className="MeteoWidget-temperature">
           <img alt="Icône de la météo actuelle" src={iconUrl}></img>
           {/* 28° */}
-          <p className="MeteoWidget-description">{description}</p> {temperature}
+          <h4 className="MeteoWidget-description">{description}</h4> {temperature}
         </h3>
       </div>
     </div>
